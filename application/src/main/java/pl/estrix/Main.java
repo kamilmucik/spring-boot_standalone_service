7aa326d330ca8eaeb7b7b4845ff427ae35d0ac7a
Kamil Mucik <kamu@gft.com>
2016-10-24 23:10:14 +0200

    settings refactor

--- application/src/main/java/pl/estrix/Main.java
+++ application/src/main/java/pl/estrix/Main.java
@@ -1,14 +1,18 @@
 package pl.estrix;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "pl.estrix")
 @EnableScheduling
+@EnableAutoConfiguration
+//@Configuration
 public class Main {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Main.class);
     }
 }
